d097e83125113fb32fed41b1ba61b72d863dcc93
sergey <sergio@dezigner.ru>
2020-09-11 18:13:02 +0300

    Функция add_order_in_db --принимает POST запросы с формы

--- RollTime/new_order/urls.py
+++ RollTime/new_order/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 
+app_name = 'new_order'
 urlpatterns = [
-    path('', views.new_order, name='new_order')
+    path('', views.new_order, name='new_order'),
+    path('add_order_in_db', views.add_order_in_db, name='add_order_in_db'),
 ]
--- RollTime/new_order/views.py
+++ RollTime/new_order/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def new_order(request):
+
+    return render(request, 'new_order/new_order.html')
+
+
+def add_order_in_db(request):
+    if request.method == 'POST':
+        print("!!!!!!!!!!!!!!!!!")
     return render(request, 'new_order/new_order.html')
